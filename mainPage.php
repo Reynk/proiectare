9b7968bdefe1ab80ecc4411b0e71066dc18ec9d0
Reynk <robertadrian.chelaru@gmail.com>
2023-10-23 22:19:00 +0300

    add main page and styling

---
+++ mainPage.php
@@ -0,0 +1,34 @@
+<?php
+echo '<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Event Tickets</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <header>
+        <nav>
+            <ul class="nav-links">
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Events</a></li>
+                <li><a href="#">Tickets</a></li>
+                <li><a href="#">Contact</a></li>
+            </ul>
+        </nav>
+    </header>
+    <main>
+        <h1>Welcome to RRZ Tickets</h1>
+        <p>Find and purchase tickets for your favorite events.</p>
+        <div class="event-list">
+            <!-- Event cards go here -->
+        </div>
+    </main>
+    <footer>
+        <p>&copy; 2023 RRZ Tickets</p>
+    </footer>
+    
+</body>
+</html>';
+?>
