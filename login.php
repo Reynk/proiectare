19022b3e743b97186cf7f7a85d9bf98fe86340ab
Reynk <robertadrian.chelaru@gmail.com>
2023-11-01 18:26:12 +0200

    add login user functionality

--- login.php
+++ login.php
@@ -8,21 +8,39 @@ function login() {
         echo $username;
         echo $password;
 
-        include 'dbConnect.php';
 
+        include 'dbConnect.php';
+        
         // Validate the input
         if (empty($username) || empty($password)) {
             echo "Please enter a username and password";
         } else {
             // Check if the username and password are correct
             /* TODO: IMPLEMENT CHECK AGAINST THE DB */
-            $sql = "SELECT * FROM admins WHERE username='$username' AND password='$password'";
-            $result = mysqli_query($conn, $sql);
 
+            // Define sql query for admins table
+            $sql = "SELECT * FROM admins WHERE username='$username' AND password='$password'";
+            $result = mysqli_query($dbConnection, $sql);
+            
+            // Attempt admin login
             if (mysqli_num_rows($result) == 1) {
                 echo "Login successful";
+                // Redirect to admin page
+                header("Location: adminPage.php");
+                exit();
+
             } else {
-                echo "Incorrect username or password";
+                // Attempt user login
+                $sql = "SELECT * FROM users WHERE username='$username' AND password='$password'";
+                $result = mysqli_query($dbConnection, $sql);
+                if (mysqli_num_rows($result) == 1) {
+                    echo "Login successful";
+                    header("Location: userPage.php");
+                } else {
+                    // Login failed
+                    echo "Incorrect username or password";
+                }
+                
             }
         }
     }
