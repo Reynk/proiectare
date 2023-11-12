f6b09766c3637e7d68f0ae273368df0c8b77fe34
Rassssven <dtimonea@gmail.com>
2023-11-12 16:21:01 +0100

    ContactPage
    Added contact page

--- contact.php
+++ contact.php
@@ -48,9 +48,59 @@ $result = mysqli_query($dbConnection, $sql);
         </div>
     </nav>
 </header>
-<main>
-    <h1>CONTACT PAGE</h1>
+
+<main class="pagina-ctc">
+
+    <h1>We'd love to hear from you!</h1>
+
 </main>
+
+
+    <div class="contact-box">
+        <h2>Contact Information</h2>
+        <span>Email: RRZ@gmail.com</span> <br>
+        <span>Address: Str. Teodor Mihali, Cluj-Napoca, Romania</span> <br>
+        <span>Telephone: 0751622683</span> <br>
+        <span>Additional Information: You can contact us by sending an email to the adress, or by calling.</span> <br>
+        <br>
+    </div>
+
+<div class="submit-box">
+    <h2>Contact Form</h2>
+    <span> Contact us! Complete this form and we will get back to you in approx 48 hours. </span> <br>
+    <span> If you still have questions, feel free to send an email to the specified adress. </span>
+
+    <form action="submit.php" method="post">
+        <div class="form-group">
+            <label for="first_name">First Name</label>
+            <input type="text" id="input-1" name="first_name" required>
+        </div>
+        <div class="form-group">
+            <label for="last_name">Last Name</label>
+            <input type="text" id="input-1" name=last_name" required>
+        </div>
+        <div class="form-group">
+            <label for="email">Email Address</label>
+            <input type="email" id="input-1" name="email" required>
+        </div>
+        <div class="form-group">
+            <label for="phone">Phone</label>
+            <input type="text" id="input-1" name="phone">
+        </div>
+        <div class="form-group">
+            <label for="message">Message</label>
+            <textarea id="input-1" name="message" rows="3" required></textarea>
+        </div>
+        <div class="form-group">
+            <label for="country">Country</label>
+            <input type="text" id="input-1" name="country">
+        </div>
+
+        <br>
+        <button class="custom-button" type="submit">Submit</button> <br>
+    </form>
+</div>
+
 <footer class="footer">
     <p>&copy; 2023 RRZ Tickets</p>
 </footer>
